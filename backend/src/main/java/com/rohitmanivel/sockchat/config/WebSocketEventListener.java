ef62b6746aa8ed9f88b7510ac8f6f9cfd2063b5f
rohit-iwnl <rohitmanivel9@gmail.com>
2025-11-10 23:24:17 -0700

    add websocket config

---
+++ backend/src/main/java/com/rohitmanivel/sockchat/config/WebSocketEventListener.java
@@ -0,0 +1,18 @@
+package com.rohitmanivel.sockchat.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j //Logging
+public class WebSocketEventListener {
+    public void handleWebSocketDisconnectListener(
+            SessionDisconnectEvent event
+    ) {
+        // TODO : handle disconnect event here
+    }
+}
