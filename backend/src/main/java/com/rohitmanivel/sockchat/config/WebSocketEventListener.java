bd586f6f08876be3e23be783a6b70cd27b270696
rohit-iwnl <rohitmanivel9@gmail.com>
2025-11-10 23:40:41 -0700

    setup disconnect event when user leaves chat

--- backend/src/main/java/com/rohitmanivel/sockchat/config/WebSocketEventListener.java
+++ backend/src/main/java/com/rohitmanivel/sockchat/config/WebSocketEventListener.java
@@ -1,7 +1,12 @@
 package com.rohitmanivel.sockchat.config;
 
+import com.rohitmanivel.sockchat.chat.ChatMessage;
+import com.rohitmanivel.sockchat.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -10,9 +15,26 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 @Slf4j //Logging
 public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
     public void handleWebSocketDisconnectListener(
             SessionDisconnectEvent event
     ) {
-        // TODO : handle disconnect event here
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String userName = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if(userName != null){
+            log.info("User disconnected : {}", userName);
+
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(userName)
+                    .build();
+
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
