ecead6f4cdd518c23812b9e0c64389b63c55b07a
rohit-iwnl <rohitmanivel9@gmail.com>
2025-11-10 23:15:02 -0700

    initial commit

---
+++ backend/src/main/java/com/rohitmanivel/sockchat/SockchatApplication.java
@@ -0,0 +1,13 @@
+package com.rohitmanivel.sockchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SockchatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rohitmanivel/sockchat/SockchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohitmanivel.sockchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
